4f8fff41a0d7644fe7e7def62fd5cc30a45c4f43
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-20 17:38:25 -0600

    seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
+  has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,21 @@
 
+
+
+20.times do
+  User.create(
+    name: Faker::TheFreshPrinceOfBelAir.character
+    nickname: Faker::TwinPeaks.character
+    email: Faker::Internet.free_email
+    encrypted_password: Faker::Internet.password
+  )
+
+5.times do
+  Post.create(
+  name: Faker::Pokemon.move
+  body: Faker::TwinPeaks.quote
+)
+  end
+end
+
+
+
