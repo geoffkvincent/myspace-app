a6f5ad31a8cca403a2190133822b65de41d26501
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-21 16:03:16 -0600

    added user id to posts, seed works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_222410) do
+ActiveRecord::Schema.define(version: 2018_09_21_215841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_09_20_222410) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 2018_09_20_222410) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,19 @@
 
 
 20.times do
-  User.create(
+  user = User.create(
     name: Faker::TheFreshPrinceOfBelAir.character,
     nickname: Faker::TwinPeaks.character,
     email: Faker::Internet.free_email,
-    encrypted_password: Faker::Internet.password,
+    password: 'password',
   )
 
-# 5.times do
-#   Post.create(
-#   name: Faker::Pokemon.move,
-#   body: Faker::TwinPeaks.quote,
-# )
-#   end
+    5.times do
+      user.posts.create(
+      name: Faker::Pokemon.move,
+      body: Faker::TwinPeaks.quote,
+    )
+  end
 end
 
 
