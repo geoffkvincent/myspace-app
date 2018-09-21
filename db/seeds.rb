13ad5090342d020d9c1e865ef0cfb95c31cb95c7
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-21 09:26:06 -0600

    post components

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,18 +3,18 @@
 
 20.times do
   User.create(
-    name: Faker::TheFreshPrinceOfBelAir.character
-    nickname: Faker::TwinPeaks.character
-    email: Faker::Internet.free_email
-    encrypted_password: Faker::Internet.password
+    name: Faker::TheFreshPrinceOfBelAir.character,
+    nickname: Faker::TwinPeaks.character,
+    email: Faker::Internet.free_email,
+    encrypted_password: Faker::Internet.password,
   )
 
-5.times do
-  Post.create(
-  name: Faker::Pokemon.move
-  body: Faker::TwinPeaks.quote
-)
-  end
+# 5.times do
+#   Post.create(
+#   name: Faker::Pokemon.move,
+#   body: Faker::TwinPeaks.quote,
+# )
+#   end
 end
 
 
